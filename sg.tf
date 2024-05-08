314766270044f28738864a697e2788e7444436f7
Kenslor Ralph Donald HYACINTHE <hyacinthe.store@gmail.com>
2024-05-08 14:50:29 -0400

    new code

--- sg.tf
+++ sg.tf
@@ -1,4 +1,5 @@
 
+#creation of a security group for the vpc
 resource "aws_security_group" "sg1" {
     name = "Terraform-sg"
     description = "Allow ssh and httpd"
@@ -7,11 +8,11 @@ resource "aws_security_group" "sg1" {
     
     ingress {
         description = "allow http"
-        from_port = 80
+        from_port = 80   # http port= 80 not secure prot if we want HTTPS we should use port= 443
         to_port = 80
         protocol = "tcp"
         #cidr_blocks = ["0.0.0.0/0"]
-        security_groups = [ aws_security_group.sg2.id ]
+        security_groups = [ aws_security_group.sg2.name ]
     }
     egress {
         from_port = 0
@@ -26,6 +27,7 @@ resource "aws_security_group" "sg1" {
 
   
 }
+#create a security group for the load balancer
 resource "aws_security_group" "sg2" {
     name = "Terraform-sg-lb"
     description = "Allow ssh and httpd"
@@ -37,6 +39,7 @@ resource "aws_security_group" "sg2" {
         to_port = 80
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
+        # Allow traffic only from the security group sg1       
     }
     egress {
         from_port = 0
@@ -46,5 +49,6 @@ resource "aws_security_group" "sg2" {
     }
   tags= {
     env = "Dev"
+    created-by-terraform = "yes"
   } 
 }
--- vpc.tf
+++ vpc.tf
@@ -1,103 +1,132 @@
-
+# Define the VPC
 resource "aws_vpc" "vpc1" {
- cidr_block = "192.168.0.0/16" 
- instance_tenancy = "default"
- tags = {
+  cidr_block       = "192.168.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
     Name = "Terraform-vpc"
-    env = "dev"
+    env  = "dev"
     Team = "DevOps"
- }
+  }
 }
+
+# Create an internet gateway and attach it to the VPC
 resource "aws_internet_gateway" "gwy1" {
   vpc_id = aws_vpc.vpc1.id
-}
-# public subnet
-resource "aws_subnet" "public1" {
-    availability_zone = "us-east-1a"
-    cidr_block = "192.168.1.0/24"
-    vpc_id = aws_vpc.vpc1.id
-    tags={
-        Name = "public-subnet-1"
-        env = "dev"
-    }
-  
-}
-resource "aws_subnet" "public2" {
-    availability_zone = "us-east-1b"
-    cidr_block = "192.168.2.0/24"
-    vpc_id = aws_vpc.vpc1.id
-    tags={
-        Name = "public-subnet-2"
-        env = "dev"
-    }
-  
-}
-#Private subnet
 
+  tags = {
+    Name = "InternetGateway"
+    env  = "dev"
+  }
+}
+
+# Create public subnets
+resource "aws_subnet" "public1" {
+  availability_zone = "us-east-1a"
+  cidr_block       = "192.168.1.0/24"
+  vpc_id           = aws_vpc.vpc1.id
+
+  tags = {
+    Name = "public-subnet-1"
+    env  = "dev"
+  }
+}
+
+resource "aws_subnet" "public2" {
+  availability_zone = "us-east-1b"
+  cidr_block       = "192.168.2.0/24"
+  vpc_id           = aws_vpc.vpc1.id
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-2"
+    env  = "dev"
+  }
+}
+
+# Create private subnets
 resource "aws_subnet" "private1" {
-    availability_zone = "us-east-1a"
-    cidr_block = "192.168.3.0/24"
-    vpc_id = aws_vpc.vpc1.id
-    tags={
-        Name = "private-subnet-1"
-        env = "dev"
-    } 
+  availability_zone = "us-east-1a"
+  cidr_block       = "192.168.3.0/24"
+  vpc_id           = aws_vpc.vpc1.id
+
+  tags = {
+    Name = "private-subnet-1"
+    env  = "dev"
+  }
 }
+
 resource "aws_subnet" "private2" {
-    availability_zone = "us-east-1b"
-    cidr_block = "192.168.4.0/24"
-    vpc_id = aws_vpc.vpc1.id
-    tags={
-        Name = "private-subnet-2"
-        env = "dev"
-    }
-  
+  availability_zone = "us-east-1b"
+  cidr_block       = "192.168.4.0/24"
+  vpc_id           = aws_vpc.vpc1.id
+
+  tags = {
+    Name = "private-subnet-2"
+    env  = "dev"
+  }
 }
-#Elastic ip and Nat gateway
-resource "aws_eip" "eip" {
-  
-}
+
+# Create an Elastic IP for the NAT gateway
+resource "aws_eip" "eip" {}
+
+# Create a NAT gateway in the public subnet and associate it with an Elastic IP
 resource "aws_nat_gateway" "nat1" {
   allocation_id = aws_eip.eip.id
-  subnet_id = aws_subnet.public1.id
-}
-#Public route table
+  subnet_id     = aws_subnet.public1.id
 
+  tags = {
+    Name = "NATGateway"
+    env  = "dev"
+  }
+}
+
+# Create a public route table and associate it with the internet gateway
 resource "aws_route_table" "rtpublic" {
- vpc_id = aws_vpc.vpc1.id 
- route {
+  vpc_id = aws_vpc.vpc1.id
+
+  route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gwy1.id
- }
+  }
+
+  tags = {
+    Name = "PublicRouteTable"
+    env  = "dev"
+  }
 }
 
-#Private route
-
+# Create a private route table and associate it with the NAT gateway
 resource "aws_route_table" "rtprivate" {
- vpc_id = aws_vpc.vpc1.id 
- route {
+  vpc_id = aws_vpc.vpc1.id
+
+  route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat1.id
- }
+    nat_gateway_id = aws_nat_gateway.nat1.id
+  }
+
+  tags = {
+    Name = "PrivateRouteTable"
+    env  = "dev"
+  }
 }
 
-## Subnet association
-
+# Associate route tables with subnets
 resource "aws_route_table_association" "rta1" {
-  subnet_id = aws_subnet.private1.id
+  subnet_id      = aws_subnet.private1.id
   route_table_id = aws_route_table.rtprivate.id
 }
+
 resource "aws_route_table_association" "rta2" {
-  subnet_id = aws_subnet.private2.id
+  subnet_id      = aws_subnet.private2.id
   route_table_id = aws_route_table.rtprivate.id
 }
 
 resource "aws_route_table_association" "rta3" {
-  subnet_id = aws_subnet.public1.id
-  route_table_id = aws_route_table.rtpublic.id
-}
-resource "aws_route_table_association" "rta4" {
-  subnet_id = aws_subnet.public2.id
+  subnet_id      = aws_subnet.public1.id
   route_table_id = aws_route_table.rtpublic.id
 }
 
+resource "aws_route_table_association" "rta4" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.rtpublic.id
+}
