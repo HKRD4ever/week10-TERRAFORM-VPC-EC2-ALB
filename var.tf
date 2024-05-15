4fb04a555aa058fbd16979a9c965aa20d113549a
Kenslor Ralph Donald HYACINTHE <hyacinthe.store@gmail.com>
2024-05-15 07:39:39 -0400

    new file modified

---
+++ var.tf
@@ -0,0 +1,15 @@
+variable "ENVIRONMENT" {
+  
+}
+
+variable "REGION" {
+    variable "REGION" {
+  type    = string
+  default = "us-west-2" // Default region, replace with your desired region
+}
+
+}
+
+variable "INSTANCE_TYPE" {
+  
+}
